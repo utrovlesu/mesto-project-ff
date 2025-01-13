088138837f78b5329db0701f4cc1b40504aa4bf8
utrovlesu <utro_v_lesu@list.ru>
2025-01-13 19:50:43 +0300

    final check

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardList= document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+initialCards.forEach((element) => {  
+    
+    cardList.append(addCards(element.link,  element.name, deleteCard));
 
-// @todo: Функция создания карточки
+});
 
-// @todo: Функция удаления карточки
+function addCards(cardName, cardImage, callback) {
 
-// @todo: Вывести карточки на страницу
+    const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardItem.querySelector('.card__image').src  = cardName;
+    cardItem.querySelector('.card__title').textContent  = cardImage;
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', callback)
+   return cardItem;
+
+}
+    
+function deleteCard(event) {
+
+   const cardItem = event.target.closest('.places__item');
+   cardItem.remove();
+
+}
